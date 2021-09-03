3beb41d0e17f653ad09134f9bb522ff6d6246f0c
Callum Dixon <callum.dixon002@gmail.com>
2021-09-03 03:53:20 -0700

    Initial Commit

---
+++ coinflip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coinflip
+//
+//  Created by Callum Dixon on 03/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
